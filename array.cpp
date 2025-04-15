a29df8b89a9b3b4e3d0257d2606e6328b21f35b7
paw2kk <wndxakk@gmail.com>
2025-04-15 10:50:17 +0700

    membuat array dengan ukuran

--- array.cpp
+++ array.cpp
@@ -4,6 +4,9 @@ using namespace std;
 //deklrasi array tanpa ukuran, wajib langsung diisi data
 int nilai[]={1,2,3};
 
+//deklarasi array dengan ukuran, dan diberi nilai awal
+int skor[4]={9,13,20,11};
+
 int main(){
 
 }
