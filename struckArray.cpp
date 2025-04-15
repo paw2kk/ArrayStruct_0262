cb47f2d1d6471fbb867a378e806bcab6fd5c5f5c
paw2kk <wndxakk@gmail.com>
2025-04-15 11:38:01 +0700

    menyelesaikan looping for dan membenarkan error

--- struckArray.cpp
+++ struckArray.cpp
@@ -17,18 +17,27 @@ struct mahasiswa
 int main(){
   mahasiswa mhs[2];
 
-  for (int i =0; i < 2; i++){
+  for(int i =0; i < 2; i++){
     cout << "Mahasiswa ke-" << i+1 << endl;
     cout << "Masukkan NIM = " ;
     cin >> mhs[i].nim;
     cin.ignore();
     cout << "Masukkan NAMA = ";
     getline(cin , mhs[i].nama);
-    coout << "Masukkan Alamat Desa = ";
+    cout << "Masukkan Alamat Desa = ";
     cin >> mhs[i].alamat.desa;
-    coout << "Masukkan Alamat Kota = ";
+    cout << "Masukkan Alamat Kota = ";
     cin >> mhs[i].alamat.kota;
-    
   }
+  
+   //Menampilkan data struct
+   for(int i = 0;i < 2;i++){
+    cout << endl;
+    cout << "Mahasiswa ke-" << i+1 << endl;
+    cout << "NIM = " << mhs[i].nim << endl;
+    cout << "Nama = " << mhs[i].nama << endl;
+    cout << "Desa = " << mhs[i].alamat.desa << endl;
+    cout << "kota = " << mhs[i].alamat.kota << endl;
+   }
 
 }
