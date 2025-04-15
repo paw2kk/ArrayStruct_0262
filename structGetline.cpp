a22265937c11a2651e54652f782a2f439be8cfa6
paw2kk <wndxakk@gmail.com>
2025-04-15 11:23:49 +0700

    menambahkan get ignore pada line 22

---
+++ structGetline.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+struct detailAlamat
+{
+    string desa;
+    string kota;
+};
+
+struct mahasiswa
+{
+    string nim;
+    string nama;
+    detailAlamat alamat;
+};
+
+int main(){
+  mahasiswa mhs;
+
+  cout << "Masukkan Nim = ";
+  cin >> mhs.nim;
+  cin.ignore();
+  cout << "Masukkan NAMA = ";
+  cin >> mhs.nama;
+  cout << "Masukkan Alamat Desa = ";
+  cin >> mhs.alamat.desa;
+  cout << "Masukkan Alamat Kota = ";
+  cin >> mhs.alamat.kota;
+
+  //menampilkan data struct
+  cout << endl;
+  cout << "NIM = " << mhs.nim << endl;
+  cout << "Nama = " << mhs.nama << endl;
+  cout << "Desa = " << mhs.alamat.desa << endl;
+  cout << "kota = " << mhs.alamat.kota << endl;
+}
